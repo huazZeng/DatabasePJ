6fdfe7a1b4fa9f3eba6c6a7d97867451da725d6b
zenghuazheng <1822636765@qq.com>
2024-05-21 11:51:47 +0800

    user all find

--- src/main/java/org/example/springboot/controller/UserController.java
+++ src/main/java/org/example/springboot/controller/UserController.java
@@ -1,7 +1,14 @@
 package org.example.springboot.controller;
 
+import org.example.springboot.entity.User;
+import org.example.springboot.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,8 +18,16 @@ import org.springframework.stereotype.Controller;
  * @author hzz
  * @since 2024-05-20
  */
-@Controller
+@RestController
 @RequestMapping("/springboot/user")
 public class UserController {
+    @Autowired
+    IUserService iUserService;
+    @GetMapping("/all")
+    public List<User> findall(){
+        System.out.println('1');
+        return  iUserService.list();
+    }
+
 
 }
